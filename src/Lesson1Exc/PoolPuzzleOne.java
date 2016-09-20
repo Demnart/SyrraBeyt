e986b44f5e5266bbeafe646ffc7a5764140e0cd3
Demnart <insidia93@gmail.com>
2016-09-20 17:39:55 +0300

    Головоломка не выполнена ;(

---
+++ src/Lesson1Exc/PoolPuzzleOne.java
@@ -0,0 +1,27 @@
+package Lesson1Exc;
+
+/**
+ * Created by Артем on 20.09.2016.
+ */
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 4) {
+            System.out.print("a ");
+            if (x < 1) {
+                System.out.println("noise");
+                x = x + 1;
+            }
+            if (x > 0) {
+                System.out.println("annoys");
+            }
+
+            if (x == 1) {
+                System.out.println("an");
+
+            }
+
+        }
+    }
+}
