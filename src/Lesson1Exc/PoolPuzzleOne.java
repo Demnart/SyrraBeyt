4d62ffe719eb4e9ab4eead2cedfd712c5d26a0e2
Demnart <insidia93@gmail.com>
2016-09-20 17:40:35 +0300

    Головоломка не выполнена ;(

--- src/Lesson1Exc/PoolPuzzleOne.java
+++ src/Lesson1Exc/PoolPuzzleOne.java
@@ -5,23 +5,24 @@ package Lesson1Exc;
  */
 public class PoolPuzzleOne {
     public static void main(String[] args) {
-        int x = 0;
-
-        while (x < 4) {
-            System.out.print("a ");
-            if (x < 1) {
-                System.out.println("noise");
-                x = x + 1;
-            }
-            if (x > 0) {
-                System.out.println("annoys");
-            }
-
-            if (x == 1) {
-                System.out.println("an");
-
-            }
-
-        }
+//        int x = 0;
+//
+//        while (x < 4) {
+//            System.out.print("a ");
+//            if (x < 1) {
+//                System.out.println("noise");
+//                x = x + 1;
+//            }
+//            if (x > 0) {
+//                System.out.println("annoys");
+//            }
+//
+//            if (x == 1) {
+//                System.out.println("an");
+//
+//            }
+//
+//        }
+        //TODO пересмотреть и закончить головоломку
     }
 }
