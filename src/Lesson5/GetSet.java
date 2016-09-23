b5efd15067a67fb18cc08369c0c084b036d32569
Genjo <insidia93@gmail.com>
2016-09-23 16:43:49 +0300

    Переменные методов всегда хранят значение для примитивов 0, для ссылок на обьект null

---
+++ src/Lesson5/GetSet.java
@@ -0,0 +1,17 @@
+package Lesson5;
+
+/**
+ * Created by Артем on 23.09.2016.
+ */
+public class GetSet {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/Lesson5/Main.java
@@ -0,0 +1,12 @@
+package Lesson5;
+
+/**
+ * Created by Артем on 23.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        GetSet getSet = new GetSet();
+        System.out.println("целочисленное значение равно: " + getSet.getAge());
+        System.out.println("Строковое значение равно: " + getSet.getName());
+    }
+}
